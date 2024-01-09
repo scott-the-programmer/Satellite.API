b2c5ea35b756235058774541396a0f69bf72fdd2
Scott Murray <scott@murray.kiwi>
2024-01-09 09:12:40 +0000

    default to port 80

--- Satellite.API/Program.cs
+++ Satellite.API/Program.cs
@@ -43,6 +43,9 @@ builder.Services.AddSingleton<ISatelliteService, SatelliteService>(o =>
 });
 #pragma warning restore CS8604 // Possible null reference argument.
 
+builder.WebHost
+    .UseUrls("http://0.0.0.0:80");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
